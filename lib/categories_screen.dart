acf6fbab9eb409fba0be8f7abc02002141d9155d
santiracca <santiagoracca7@gmail.com>
2020-04-09 18:47:14 -0400

    Add basic navigation

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -11,8 +11,10 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(catData.title, catData.color))
+            .map((catData) =>
+                CategoryItem(catData.title, catData.color, catData.id))
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './categories_screen.dart';
+import './category_meals_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
-      home: CategoriesScreen(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                headline6: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'RobotoCondensed',
+                    fontWeight: FontWeight.bold),
+              ),
+        ),
+        // home: CategoriesScreen(),
+        routes: {
+          "/": (ctx) => CategoriesScreen(),
+          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        });
   }
 }
